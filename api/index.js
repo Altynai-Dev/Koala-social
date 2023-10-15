42990c4b949ee70927d6a780d33cb7258595281f
Altynai-Dev <altynai.imnl@gmail.com>
2023-10-15 11:51:48 -0400

    api comments

--- api/index.js
+++ api/index.js
@@ -4,9 +4,12 @@ import authRoutes from './routes/auth.js';
 import userRoutes from './routes/users.js';
 import postRoutes from './routes/posts.js';
 import commentsRoutes from './routes/comments.js';
+import likesRoutes from './routes/likes.js';
+import relationshipRoutes from './routes/relationships.js';
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import multer from 'multer';
+
 
 //middleware
 app.use((req, res, next)=>{
@@ -19,40 +22,50 @@ app.use(cors({
     origin: "http://localhost:3000"
 }));
 app.use(cookieParser());
-app.use('/uploads', express.static('uploads'))
-
-// const storage = multer.diskStorage({
-//     destination: function (req, file, cb) {
-//       cb(null, "../client/public/upload");
-//     },
-//     filename: function (req, file, cb) {
-//       cb(null, Date.now() + file.originalname);
-//     },
-// });
+//app.use('../client/public/upload', express.static('upload'))
 
 const storage = multer.diskStorage({
-    destination: (_, __, cb)=>{
-        cb(null, "uploads")
+    destination: function (req, file, cb) {
+      cb(null, "../client/public/upload");
     },
-    filename: (_, file, cb)=>{
-        cb(null, file.originalname)
-    }
-})
+    filename: function (req, file, cb) {
+      cb(null, Date.now() + file.originalname);
+    },
+  });
+  
+  const upload = multer({ storage: storage });
+  
+  app.post("/api/upload", upload.single("file"), (req, res) => {
+    const file = req.file;
+    res.status(200).json(file.filename);
+  });
 
-const upload = multer({ storage: storage });
+// const storage = multer.diskStorage({
+//     destination: (_, __, cb)=>{
+//         cb(null, "uploads")
+//     },
+//     filename: (_, file, cb)=>{
+//         cb(null, file.originalname)
+//     }
+// })
 
-app.post("/api/upload", upload.single("file"), (req, res) => {
-    // const file = req.file;
-    // res.status(200).json(file.filename);
-    res.json({
-        url: `uploads/${req.file.originalname}`
-    })
-});
+// const upload = multer({ storage: storage });
+
+// app.post("/api/upload", upload.single("file"), (req, res) => {
+//     // const file = req.file;
+//     // res.status(200).json(file.filename);
+//     res.json({
+//         url: `uploads/${req.file.originalname}`
+//     })
+// });
 
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/posts', postRoutes);
 app.use('/api/comments', commentsRoutes);
+app.use('/api/likes', likesRoutes);
+app.use("/api/relationships", relationshipRoutes);
+
 app.get('/', (req, res)=>{
     res.send('working good')
 })
